885dd24db6a45252c76fc275ae7f8ee97f0b53cf
Ali Ismoilov <125017239+AliIsmoilov@users.noreply.github.com>
2026-02-10 11:16:18 +0100

    DockerFile fix

--- migrations/000002_users.up.sql
+++ migrations/000002_users.up.sql
@@ -1,7 +1,12 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
+    password TEXT NOT NULL,
     role TEXT NOT NULL, -- admin, user, etc
     created_at TIMESTAMP DEFAULT now()
 );
+
+SELECT * FROM users
+-- INSERT INTO public.users(
+-- 	id, email, password, role, created_at)
+-- 	VALUES ('f07378d0-3fd4-42a0-b8fd-dde73b0601a6', 'travelxon@gmail.com', 'Tester@123', 'admin', now());
